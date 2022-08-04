d96043baca2b2ea44559f667962c4e878f4da1e6
Diogo Soares <diogo.soares@solarview.com.br>
2022-08-04 09:56:33 -0300

    Primeiro commit

---
+++ src/contexts/CyclesContext.tsx
@@ -0,0 +1,132 @@
+import { differenceInSeconds } from 'date-fns'
+import {
+  createContext,
+  ReactNode,
+  useEffect,
+  useReducer,
+  useState,
+} from 'react'
+
+// IMPORT REDUCERS
+import { Cycle, cyclesReducer } from '../reducers/cycles/reducer'
+import {
+  addNewCyclesAction,
+  interruptCurrentCycleAction,
+  markCurrentCycleAsFinishedAction,
+} from '../reducers/cycles/actions'
+
+const nameStorageData = '@ignite-timer:cycles-state-1.0.0'
+const minTime = 5
+const maxTime = 60
+
+interface CreateCycleData {
+  task: string
+  minutesAmount: number
+}
+
+interface CyclesContextType {
+  cycles: Cycle[]
+  activeCycle: Cycle | undefined
+  activeCycleId: string | null
+  maxTime: number
+  minTime: number
+  amountSecondsPassed: number
+  markCurrentCycleAsFinished: () => void
+  setSecondsPassed: (seconds: number) => void
+  createNewCycle: (data: CreateCycleData) => void
+  interruptCurrentCycle: () => void
+}
+
+interface CyclesContextProviderProps {
+  children: ReactNode
+}
+
+// CONTEXT
+export const CyclesContext = createContext({} as CyclesContextType)
+
+export function CyclesContextProvider({
+  children,
+}: CyclesContextProviderProps) {
+  const [cyclesState, dispatch] = useReducer(
+    cyclesReducer,
+    {
+      cycles: [],
+      activeCycleId: null,
+    },
+    () => {
+      const storageStateAsJSON = localStorage.getItem(nameStorageData)
+
+      if (storageStateAsJSON) {
+        return JSON.parse(storageStateAsJSON)
+      }
+
+      return {
+        cycles: [],
+        activeCycleId: null,
+      }
+    },
+  )
+
+  const { cycles, activeCycleId } = cyclesState
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+    if (activeCycle) {
+      return differenceInSeconds(new Date(), new Date(activeCycle.startDate))
+    }
+
+    return 0
+  })
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState)
+
+    localStorage.setItem(nameStorageData, stateJSON)
+  }, [cyclesState])
+
+  function setSecondsPassed(seconds: number) {
+    setAmountSecondsPassed(seconds)
+  }
+
+  function markCurrentCycleAsFinished() {
+    dispatch(markCurrentCycleAsFinishedAction())
+  }
+
+  function createNewCycle(data: CreateCycleData) {
+    const id = String(new Date().getTime())
+
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+      startDate: new Date(),
+    }
+
+    dispatch(addNewCyclesAction(newCycle))
+
+    setAmountSecondsPassed(0)
+  }
+
+  function interruptCurrentCycle() {
+    dispatch(interruptCurrentCycleAction())
+  }
+
+  return (
+    <CyclesContext.Provider
+      value={{
+        activeCycle,
+        activeCycleId,
+        amountSecondsPassed,
+        createNewCycle,
+        cycles,
+        interruptCurrentCycle,
+        maxTime,
+        minTime,
+        markCurrentCycleAsFinished,
+        setSecondsPassed,
+      }}
+    >
+      {children}
+    </CyclesContext.Provider>
+  )
+}
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,23 @@
+export const defaultTheme = {
+  white: '#FFF',
+
+  'gray-100': '#E1E1E6',
+  'gray-300': '#C4C4CC',
+  'gray-400': '#8D8D99',
+  'gray-500': '#7C7C8A',
+  'gray-600': '#323238',
+  'gray-700': '#29292E',
+  'gray-800': '#202024',
+  'gray-900': '#121214',
+
+  'primary-300': '#9FE4FF',
+  'primary-500': '#70D6FF',
+  'primary-700': '#07B8FF',
+
+  'green-success': '#04D361',
+  'yellow-progress': '#FBA94C',
+  'red-interrupted': '#AB222E',
+
+  'danger-500': '#AB222E',
+  'danger-700': '#7A1921',
+}
